abdbe7858f6200e3f9636e022a629dc306ed572e
Kirill Kvitkovski <kirill2ll@mail.ru>
2023-05-09 14:54:05 +0300

    Update 02.staced_queries.py

--- Python_advanced/04.stacks_and_queues/02.staced_queries.py
+++ Python_advanced/04.stacks_and_queues/02.staced_queries.py
@@ -1,3 +1,11 @@
+# You have an empty stack. You will receive an integer – N. On the next N lines, you will receive queries. Each query is one of these four types:
+# •	'1 {number}' – push the number (integer) into the stack
+# •	'2' – delete the number at the top of the stack
+# •	'3' – print the maximum number in the stack
+# •	'4' – print the minimum number in the stack
+# It is guaranteed that each query is valid.
+
+
 number_of_lines = int(input())
 stack = []
 
