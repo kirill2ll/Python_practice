9d889f938bb3d01f812a818764caf2afb0c650f8
Kirill Kvitkovski <kirill2ll@mail.ru>
2023-05-29 00:03:58 +0300

    Create 06.matrix_shuffling.py

---
+++ Python_advanced/10.multidimensional_lists/06.matrix_shuffling.py
@@ -0,0 +1,8 @@
+row, col = map(int, input().split())
+
+matrix = [[int(num) for num in input().split()] for cur_row in range(row)]
+
+print(matrix)
+for r in range(row):
+    for c in range(col):
+        pass
